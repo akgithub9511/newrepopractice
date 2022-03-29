4a41eec8647f6e30beea5da2107b16ff2e1333a8
schonde <schondegithub@gmail.com>
2022-03-29 14:52:43 +0530

    Added multiplication file

---
+++ src/Calculator/MultiolicationProgram.java
@@ -0,0 +1,22 @@
+package Calculator;
+
+public class MultiplicationProgram{
+	
+int first_Number = 0;
+int second_Number = 0;
+
+
+public int multiplication(int first ,int second) {
+	
+	
+	first_Number =first1 ;
+	
+	second_Number = second;
+	
+	return (first_Number*second_Number);
+}
+	
+
+	
+
+}
---
+++ src/Calculator_test/Multiplication_test.java
@@ -0,0 +1,15 @@
+package Calculator_test;
+
+import Calculator.AdditionProgram;
+
+public class Multiplication_test {
+
+	public static void main(String[] args) {
+		
+		MultiplicationProgram add =new MultiplicationProgram ();		
+		
+		add.multiplication(20, 30);
+		
+	}
+
+}
